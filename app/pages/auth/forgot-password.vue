a1641e154fd067e866e784fefc692f987f155be1
rolando guerra <rolandoguerra1991@gmail.com>
2025-07-16 18:42:00 -0400

    add auth password pages

--- app/pages/auth/forgot-password.vue
+++ app/pages/auth/forgot-password.vue
@@ -2,10 +2,37 @@
 definePageMeta({
   layout: 'auth'
 })
+
+const state = reactive({
+  email: ''
+})
 </script>
 
 <template>
-  <div>
-     <!-- Forgot Password Page Content -->
-  </div>
+  <UCard variant="subtle">
+    <UForm
+      :state
+      class="space-y-4">
+      <UFormField
+        label="Email"
+        name="email">
+        <UInput
+          v-model="state.email"
+          type="email"
+          class="w-full"
+          placeholder="Enter your email" />
+      </UFormField>
+
+        <UButton type="submit" block>
+          Send Reset Link
+        </UButton>
+    </UForm>
+
+    <p class="text-center mt-2">
+      Remembered your password?
+      <ULink to="/auth/login" class="text-primary">
+        Login
+      </ULink>
+    </p>
+  </UCard>
 </template>
--- app/pages/auth/password-reset.vue
+++ app/pages/auth/password-reset.vue
@@ -2,10 +2,41 @@
 definePageMeta({
   layout: 'auth'
 })
+
+const state = reactive({
+  password: '',
+  confirmPassword: ''
+})
 </script>
 
 <template>
-  <div>
-     <!-- Password Reset Page Content -->
-  </div>
+  <UCard variant="subtle">
+    <UForm
+      :state
+      class="space-y-4">
+        <UFormField
+          label="Password"
+          name="password">
+          <UInput
+            v-model="state.password"
+            type="password"
+            class="w-full"
+            placeholder="Enter new password" />
+        </UFormField>
+
+        <UFormField
+          label="Confirm Password"
+          name="confirmPassword">
+          <UInput
+            v-model="state.confirmPassword"
+            type="password"
+            class="w-full"
+            placeholder="Confirm your password" />
+        </UFormField>
+
+        <UButton type="submit" block>
+          Reset Password
+        </UButton>
+    </UForm>
+  </UCard>
 </template>
